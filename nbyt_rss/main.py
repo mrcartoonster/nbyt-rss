95ba0f7e6855ed95df4b50972940f8cec11a8c0a
Evan Baird <evanbaird@evans-mbp.lan>
2024-09-21 17:53:13 -0600

    version 4.4 working

--- nbyt_rss/main.py
+++ nbyt_rss/main.py
@@ -3,21 +3,24 @@ from pathlib import Path
 from typing import List, Optional
 
 import typer
+from rich import print
 from typing_extensions import Annotated
 
 from .__init__ import __version__
 from .getting import channel_name, getting_link
 
-app = typer.Typer()
+app = typer.Typer(rich_markup_mode="rich")
 
 
 def version_callback(value: bool):
     if value:
-        print(f"Awesome CLI Version: {__version__}")
+        print(
+            f":sparkle:[bold]Awesome CLI Version:[/] [green]{__version__}[/]",
+        )
         raise typer.Exit()
 
 
-@app.command()
+@app.command(epilog="[bold red]Made with desperation[/] :sweat:")
 def main(
     urls: List[str],
     version: Annotated[
